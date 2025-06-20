1ce1f7c0e65597da1eaac3861038a8c6df761ddc
TurkeyChans <58515738+TurkeyChans@users.noreply.github.com>
2025-06-20 02:15:34 -0400

    starting making cards and added CSS to nav

--- prog.js
+++ prog.js
@@ -1,9 +1,9 @@
 let count = 0;
 let save = 0;
-const money = document.getElementById('Money');
-setInterval(() => {
-  count++;
-  money.textContent = count;
-}, 100);
+//const money = document.getElementById('Money');
+//setInterval(() => {
+  //count++;
+  //money.textContent = count;
+//}, 100);
 
 

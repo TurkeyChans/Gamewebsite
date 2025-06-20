b611e2c6341db594fb22a5f6d407bf642bc1b51d
TurkeyChans <58515738+TurkeyChans@users.noreply.github.com>
2025-06-20 16:51:51 -0400

    Made a buy and sell button that works & more CSS

--- prog.js
+++ prog.js
@@ -1,2 +1,18 @@
-//add something here
+window.onload = openPopup;
 
+function openPopup() {
+    document.getElementById('popup').classList.add('show');
+}
+function closePopup() {
+    document.getElementById('popup').classList.remove('show');
+}
+function sell() {
+    for(let i = 1; i <= 4; ++i) {
+        document.getElementById(`Cost_Sell_${i}`).innerHTML = "Sell: ";
+    }
+}
+function buy() {
+    for(let i = 1; i <= 4; ++i) {
+        document.getElementById(`Cost_Sell_${i}`).innerHTML = "Cost: ";
+    }
+}

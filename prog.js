3a361462e21dbe3619f8d1fd61e0b92fd3de2057
TurkeyChans <58515738+TurkeyChans@users.noreply.github.com>
2025-06-20 02:19:52 -0400

    Add files via upload

--- prog.js
+++ prog.js
@@ -1,9 +1,3 @@
-let count = 0;
-let save = 0;
-//const money = document.getElementById('Money');
-//setInterval(() => {
-  //count++;
-  //money.textContent = count;
-//}, 100);
+//add something here
 
 

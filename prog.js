3538c85173d664f4c7c7d4a0763b24e85c24c8e0
TurkeyChans <58515738+TurkeyChans@users.noreply.github.com>
2025-06-20 20:13:45 -0400

    added values to cost and sell

--- prog.js
+++ prog.js
@@ -1,5 +1,6 @@
 window.onload = openPopup;
-
+let price = [100,200,300,400]
+let market = [150, 300, 500, 700]
 function openPopup() {
     document.getElementById('popup').classList.add('show');
 }
@@ -8,11 +9,11 @@ function closePopup() {
 }
 function sell() {
     for(let i = 1; i <= 4; ++i) {
-        document.getElementById(`Cost_Sell_${i}`).innerHTML = "Sell: ";
+        document.getElementById(`Cost_Sell_${i}`).innerHTML = `Sell: ${market[i-1]}`;
     }
 }
 function buy() {
     for(let i = 1; i <= 4; ++i) {
-        document.getElementById(`Cost_Sell_${i}`).innerHTML = "Cost: ";
+        document.getElementById(`Cost_Sell_${i}`).innerHTML = `Cost: ${price[i-1]}`;
     }
 }
